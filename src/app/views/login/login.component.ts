a29abf2349d4401010f75f57bc474e0f9486803a
ftd-a7 <jhonatan.leon@farmatodo.com>
2021-02-23 16:36:02 -0500

    se crean formularios reactivos registro e inicio de sesion

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  showLogin = false;
+  formLogin: FormGroup;
+  formRegister: FormGroup;
+
+  constructor( private form: FormBuilder) {
+    this.createFormLogin();
+    this.createFormRegister();
+  }
 
   ngOnInit(): void {
+  }
+
+  createFormRegister() {
+    this.formRegister = this.form.group(
+      {
+        email: ['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')] ],
+        name: ['', [ Validators.required ] ],
+        nit: ['', [ Validators.required ] ],
+        password: ['', [ Validators.required ] ]
+      }
+    )
+
+  }
+
+  createFormLogin() {
+    this.formLogin = this.form.group(
+      {
+        email: ['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')] ],
+        password: ['', [ Validators.required ] ]
+      }
+    )
+  }
+
+  login() {
+    if ( this.formLogin.valid ) {
+      console.log('ingresa ok');
+    }
+  }
+
+  register() {
+    if ( this.formRegister.valid ) {
+      console.log('registro ok');
+    }
   }
 
 }
